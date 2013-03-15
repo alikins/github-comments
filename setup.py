1f445d9e8a2c002205da9ceaec4e0b4e5ff60ced
Adrian Likins <alikins@redhat.com>
2013-03-14 23:38:00 -0400

    start adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="github-comments",
+        version='1.3',
+        url="https://github.com/alikins/github-comments",
+        description="show pull request comments in a lint like fashion",
+        author="Adrian Likins",
+        author_email="adrian@likins.com",
+        packages=["github_comments"]
+        )
